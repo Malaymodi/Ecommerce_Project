e0d56a5552a2141b5a44fceea849f6f5b7c2e065
Gurprit Singh Saluja <GurpritSinghS@tblocks.com>
2022-10-09 22:38:50 +0530

    signinin flow code

---
+++ Ecommerce_Project_WebAPI/APIRequestModels/UserRequestViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ecommerce_Project_WebAPI.APIRequestModels
+{
+    public class UserRequestViewModel
+    {
+        public class UserLoginModel
+        {
+            [Required]
+            public string Username { get; set; }
+
+            [Required]
+            public string Password { get; set; }
+
+            public bool RememberMe { get; set; }
+
+        }
+    }
+}
